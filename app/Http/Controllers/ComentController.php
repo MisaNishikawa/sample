83e85535133f755a90092cd2a4e35e355300170c
西川未紗 <>
2020-03-11 17:22:15 +0900

    コメントアウト

--- app/Http/Controllers/ComentController.php
+++ app/Http/Controllers/ComentController.php
@@ -25,14 +25,17 @@ class ComentController extends Controller
 
         public function store(UpdateComent $request)
         {
+
+            //データの取得
             $request -> all();
 
+            //create保存　All全て取得 find一つ一致するやつ delete削除
             Coment::create([
                 'body'=>$request->input('body')
             ]);
 
 //viewはファイルを読み込みたいとき。redirectは処理が終わったらどこも経由しないで帰ってくる。
-
+//画面の表示
             return redirect()->to('coments');
         }
 
